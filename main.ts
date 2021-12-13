92cc96f8e8463dbb2a34eaa2a36043bbcff19755
Josh Daniel <joshpersonal8@gmail.com>
2021-12-13 20:52:25 +0800

    chore: add support for npm v6.x

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ type PackageManager = 'npm' | 'yarn';
 const main = async () => {
   const cwd = process.cwd();
   const res = execSync('npm list -g', { stdio: 'pipe' });
+  const npmVersion = execSync('npm -v', { stdio: 'pipe' });
 
   const answers = await inquirer.prompt([
     {
@@ -49,7 +50,11 @@ const main = async () => {
   const useNpm = pkgManager === 'npm';
   const useYarn = !useNpm && res.toString().includes('yarn@');
 
-  const npmTemplate = `npm init vite@latest ${projectName} -- --template ${template}`;
+  const viteTemplate = `npm init vite@latest ${projectName}`;
+  let npmTemplate: string;
+  if (npmVersion.toString().split('.')[0] === '6')
+    npmTemplate = `${viteTemplate} --template ${template}`;
+  else npmTemplate = `${viteTemplate} -- --template ${template}`; // npm 7+, extra double-dash is needed
   const yarnTemplate = `yarn create vite ${projectName} --template ${template}`;
 
   const nextTsFlag = template === 'next-ts' ? '--ts' : '';
@@ -63,96 +68,96 @@ const main = async () => {
     return execSync(cmd, { stdio: 'inherit' });
   };
 
-try {
-  if (template.includes('next')) {
-    if (useYarn) execSync(nextYarnTemplate, { stdio: 'inherit' });
-    else if (useNpm) execSync(nextNpmTemplate, { stdio: 'inherit' });
-    else yarnNotFound(nextNpmTemplate);
-  } else {
-    if (useYarn) {
-      console.log(colors.gray('\n→ Using yarn to install packages...'));
-      execSync(yarnTemplate, { stdio: 'inherit' });
-    } else if (useNpm) {
-      console.log(colors.gray('\n→ Using npm to install packages...'));
-      execSync(npmTemplate, { stdio: 'inherit' });
-    } else yarnNotFound(npmTemplate);
+  try {
+    if (template.includes('next')) {
+      if (useYarn) execSync(nextYarnTemplate, { stdio: 'inherit' });
+      else if (useNpm) execSync(nextNpmTemplate, { stdio: 'inherit' });
+      else yarnNotFound(nextNpmTemplate);
+    } else {
+      if (useYarn) {
+        console.log(colors.gray('\n→ Using yarn to install packages...'));
+        execSync(yarnTemplate, { stdio: 'inherit' });
+      } else if (useNpm) {
+        console.log(colors.gray('\n→ Using npm to install packages...'));
+        execSync(npmTemplate, { stdio: 'inherit' });
+      } else yarnNotFound(npmTemplate);
+    }
+
+    process.chdir(path.join(cwd, projectName));
+
+    try {
+      console.log(colors.gray('\n→ Installing dependencies & Tailwind CSS...'));
+
+      let prefix = 'npm i';
+
+      if (!useNpm) prefix = 'yarn add';
+      execSync(
+        `${prefix} -D tailwindcss@latest postcss@latest autoprefixer@latest`,
+        { stdio: 'inherit' }
+      );
+
+      execSync(`npx tailwindcss init -p`, { stdio: 'inherit' });
+
+      console.log(colors.green.bold('\n[ Installed Successfully! ]'));
+      console.log('\nGet Started, Run:'.gray);
+
+      console.log(colors.yellow(`\ncd ${projectName}`));
+
+      if (useNpm) console.log(colors.yellow('npm run dev'));
+      else console.log(colors.yellow('yarn dev'));
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.error('Error occurred:', err);
   }
 
-  process.chdir(path.join(cwd, projectName));
+  const projectDir = path.join(cwd, projectName);
+
+  const useTwConfig = () => {
+    let content: string;
+
+    if (template.includes('next'))
+      content =
+        "'./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'";
+    else if (template.includes('vanilla')) content = "'./index.html'";
+    else content = "'./index.html', './src/**/*.{js,ts,jsx,tsx}'";
+
+    return `content: [${content}]`;
+  };
+
+  const useTwPath = () => {
+    switch (template) {
+      case 'next':
+      case 'next-ts':
+        return 'styles/globals.css';
+
+      case 'vanilla':
+        return 'style.css';
+
+      case 'vanilla-ts':
+        return 'src/style.css';
+
+      default:
+        return 'src/index.css';
+    }
+  };
+
+  const tailwindConfig = {
+    files: `${projectDir}/tailwind.config.js`,
+    from: /content: \[]/g,
+    to: useTwConfig(),
+  };
 
   try {
-    console.log(colors.gray('\n→ Installing dependencies & Tailwind CSS...'));
-
-    let prefix = 'npm i';
-
-    if (!useNpm) prefix = 'yarn add';
-    execSync(
-      `${prefix} -D tailwindcss@latest postcss@latest autoprefixer@latest`,
-      { stdio: 'inherit' }
+    await replaceInFile(tailwindConfig);
+    fs.writeFileSync(
+      path.join(projectDir, useTwPath()),
+      '@tailwind base;\n@tailwind components;\n@tailwind utilities;'
     );
-
-    execSync(`npx tailwindcss init -p`, { stdio: 'inherit' });
-
-    console.log(colors.green.bold('\n[ Installed Successfully! ]'));
-    console.log('\nGet Started, Run:'.gray);
-
-    console.log(colors.yellow(`\ncd ${projectName}`));
-
-    if (useNpm) console.log(colors.yellow('npm run dev'));
-    else console.log(colors.yellow('yarn dev'));
   } catch (err) {
-    console.log(err);
+    console.error('Error occurred:', err);
   }
-} catch (err) {
-  console.error('Error occurred:', err);
-}
-
-const projectDir = path.join(cwd, projectName);
-
-const useTwConfig = () => {
-  let content: string;
-
-  if (template.includes('next'))
-    content =
-      "'./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'";
-  else if (template.includes('vanilla')) content = "'./index.html'";
-  else content = "'./index.html', './src/**/*.{js,ts,jsx,tsx}'";
-
-  return `content: [${content}]`;
-};
-
-const useTwPath = () => {
-  switch (template) {
-    case 'next':
-    case 'next-ts':
-      return 'styles/globals.css';
-
-    case 'vanilla':
-      return 'style.css';
-
-    case 'vanilla-ts':
-      return 'src/style.css';
-
-    default:
-      return 'src/index.css';
-  }
-};
-
-const tailwindConfig = {
-  files: `${projectDir}/tailwind.config.js`,
-  from: /content: \[]/g,
-  to: useTwConfig(),
-};
-
-try {
-  await replaceInFile(tailwindConfig);
-  fs.writeFileSync(
-    path.join(projectDir, useTwPath()),
-    '@tailwind base;\n@tailwind components;\n@tailwind utilities;'
-  );
-} catch (err) {
-  console.error('Error occurred:', err);
-}
-};
+};;
 
 main();
